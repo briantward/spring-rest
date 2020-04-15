3ede700c626b2acad0286a41894fc7da36b3c2e7
Brian Ward <bward@redhat.com>
2020-04-15 11:18:12 -0400

    long sleep

--- src/main/java/openshift/cop/Application.java
+++ src/main/java/openshift/cop/Application.java
@@ -2,14 +2,15 @@ package openshift.cop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.lang.InterruptedException;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"openshift.cop"})
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.InterruptedException {
+        Thread.sleep(600000);
         SpringApplication.run(Application.class, args);    
     }
 }
